f2fa29a48b805604fbf2afb037ea820624f7a614
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-22 09:09:37 -0700

    Remove version checks from this action
    Since we can do it in a separate action.
    Makes this action more self-contained.

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -26,34 +26,5 @@ cmd="jupyter-repo2docker --no-run --user-id 1234 --user-name ${NB_USER} --image-
 echo "repo2docker command: $cmd"
 eval $cmd
 
-echo "About to check docker path"
-docker --version
-echo "Finished checking docker path"
-
-echo "Checking docker images"
-docker image list
-docker container list
-echo "Checking docker images"
-
-echo "About to list environments"
-docker run $SHA_NAME /srv/conda/bin/conda env list
-echo "About to run docker run ${SHA_NAME} /srv/conda/bin/conda --version"
-docker run $SHA_NAME /srv/conda/bin/conda --version | cut -d " " -f 2
-echo "About to run it again to capture the output"
-CURR_CONDA_VER=`docker run $SHA_NAME /srv/conda/bin/conda --version | cut -d " " -f 2`
-echo "Finished running docker run"
-
-echo "About to run pip on the local container"
-pip3 list | grep jupyter-repo2docker
-echo "About to run it again to capture the output"
-CURR_R2D_VER=`pip3 list | grep jupyter-repo2docker`
-
-echo "Variable results = $CURR_CONDA_VER and $CURR_R2D_VER"
-
-CURR_DOCKER_VER=`docker --version`
-
 # Emit output variables
 echo "::set-output name=IMAGE_SHA_NAME::${SHA_NAME}"
-echo "::set-output name=CURR_CONDA_VER::${CURR_CONDA_VER}"
-echo "::set-output name=CURR_R2D_VER::${CURR_R2D_VER}"
-echo "::set-output name=CURR_DOCKER_VER::${CURR_DOCKER_VER}"
