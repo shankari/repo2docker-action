4977b07d0debf94e99c1905c9bde3141361cf66b
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-21 22:51:56 -0700

    Let's see if getting the version still works

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -11,13 +11,18 @@ function check_env() {
     fi
 }
 
+check_env "INPUT_IMAGE_NAME"
+
 # Pick username
 NB_USER=${NOTEBOOK_USER:-"$GITHUB_ACTOR"}
-IMG_NAME=${IMAGE_NAME:-"mobilitynet"}
-echo "Using user $NB_USER to create image name $IMG_NAME"
+
+shortSHA=$(echo "${GITHUB_SHA}" | cut -c1-12)
+SHA_NAME="${INPUT_IMAGE_NAME}:${shortSHA}"
+
+echo "Using user $NB_USER to create image name $SHA_NAME"
 
 # Run repo2docker
-cmd="jupyter-repo2docker --no-run --user-id 1234 --user-name ${NB_USER} --image-name ${IMG_NAME} --ref $GITHUB_SHA ${PWD}"
+cmd="jupyter-repo2docker --no-run --user-id 1234 --user-name ${NB_USER} --image-name ${SHA_NAME} --ref $GITHUB_SHA ${PWD}"
 echo "repo2docker command: $cmd"
 eval $cmd
 
@@ -25,9 +30,11 @@ echo "About to check docker path"
 docker --version
 echo "Finished checking docker path"
 
-echo 'About to run docker run $IMG_NAME:latest /srv/conda/bin/conda --version | cut -d " " -f 2'
-CURR_CONDA_VER=`docker run $IMG_NAME:latest /srv/conda/bin/conda --version | cut -d " " -f 2`
-# CURR_R2D_VER=`/srv/conda/envs/notebook/bin/pip list | grep jupyter-repo2docker`
+echo "About to run docker run ${SHA_NAME} /srv/conda/bin/conda --version"
+CURR_CONDA_VER=`docker run $SHA_NAME /srv/conda/bin/conda --version | cut -d " " -f 2`
+CURR_R2D_VER=`docker run $SHA_NAME /srv/conda/envs/notebook/bin/pip list | grep jupyter-repo2docker`
+
+echo "Variable results = $CURR_CONDA_VER and $CURR_R2D_VER"
 
 CURR_CONDA_VER="UNKNOWN"
 CURR_R2D_VER="UNKNOWN"
@@ -36,3 +43,4 @@ CURR_DOCKER_VER=`docker --version`
 # Emit output variables
 echo "::set-output name=CURR_CONDA_VER::${CURR_CONDA_VER}"
 echo "::set-output name=CURR_R2D_VER::${CURR_R2D_VER}"
+echo "::set-output name=CURR_DOCKER_VER::${CURR_DOCKER_VER}"
