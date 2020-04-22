b68c088315b460dd2f8504f44e05eaf3f81abe46
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-22 00:05:18 -0700

    Let's try to run the docker commands without backticks

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -30,9 +30,15 @@ echo "About to check docker path"
 docker --version
 echo "Finished checking docker path"
 
+echo "Checking docker images"
+docker image list
+docker container list
+echo "Checking docker images"
+
 echo "About to run docker run ${SHA_NAME} /srv/conda/bin/conda --version"
-CURR_CONDA_VER=`docker run $SHA_NAME /srv/conda/bin/conda --version | cut -d " " -f 2`
-CURR_R2D_VER=`docker run $SHA_NAME /srv/conda/envs/notebook/bin/pip list | grep jupyter-repo2docker`
+docker run $SHA_NAME /srv/conda/bin/conda --version | cut -d " " -f 2
+docker run $SHA_NAME /srv/conda/envs/notebook/bin/pip list | grep jupyter-repo2docker
+echo "Finished running docker run`
 
 echo "Variable results = $CURR_CONDA_VER and $CURR_R2D_VER"
 
@@ -41,6 +47,7 @@ CURR_R2D_VER="UNKNOWN"
 CURR_DOCKER_VER=`docker --version`
 
 # Emit output variables
+echo "::set-output name=IMAGE_SHA_NAME::${SHA_NAME}"
 echo "::set-output name=CURR_CONDA_VER::${CURR_CONDA_VER}"
 echo "::set-output name=CURR_R2D_VER::${CURR_R2D_VER}"
 echo "::set-output name=CURR_DOCKER_VER::${CURR_DOCKER_VER}"
